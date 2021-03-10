15eba8d16e7186a0201f788e139ef9622f1ccd7c
Ilhomidin Bahoraliev <itilhomidin@yandex.com>
2021-03-10 22:08:25 +0500

    Add tokens

---
+++ bfc/tokens.py
@@ -0,0 +1,77 @@
+from abc import ABC, abstractproperty
+from dataclasses import dataclass
+from typing import Union
+
+
+class NonValueToken(ABC):
+    @abstractproperty
+    def tag(self) -> str:
+        ...
+
+
+class ValueToken(NonValueToken, ABC):
+    @abstractproperty
+    def value(self) -> int:
+        ...
+
+
+@dataclass(frozen=True)
+class MutateCellValueToken(ValueToken):
+    value: int
+
+    @property
+    def tag(self) -> str:
+        return "MutateCellValue"
+
+
+class LoopStartToken(NonValueToken):
+    @property
+    def tag(self) -> str:
+        return "LoopStart"
+
+
+class LoopEndToken(NonValueToken):
+    @property
+    def tag(self) -> str:
+        return "LoopEnd"
+
+
+class NextCellToken(NonValueToken):
+    @property
+    def tag(self) -> str:
+        return "NextCell"
+
+
+class PreviousCellToken(NonValueToken):
+    @property
+    def tag(self) -> str:
+        return "PreviousCell"
+
+
+class PutCellValueToken(NonValueToken):
+    @property
+    def tag(self) -> str:
+        return "PutCellValue"
+
+
+class GetCellValueToken(NonValueToken):
+    @property
+    def tag(self) -> str:
+        return "GetCellValue"
+
+
+Token = Union[NonValueToken, ValueToken]
+
+
+__all__ = [
+    "Token",
+    "NonValueToken",
+    "ValueToken",
+    "MutateCellValueToken",
+    "LoopStartToken",
+    "LoopEndToken",
+    "NextCellToken",
+    "PreviousCellToken",
+    "PutCellValueToken",
+    "GetCellValueToken",
+]
