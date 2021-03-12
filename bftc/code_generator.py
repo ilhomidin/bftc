6f88953f3edd92a4aec6fc7e62c16d2e7a90af4b
Ilhomidin Bahoraliev <itilhomidin@yandex.com>
2021-03-13 00:35:17 +0500

    Fix code_generator

--- bftc/code_generator.py
+++ bftc/code_generator.py
@@ -1,7 +1,10 @@
+from string import Template
+
 from .tokens import Token
 from .transpiler import transpile
 
-C_CODE = """
+C_CODE = Template(
+"""
 #include <stdio.h>
 
 
@@ -9,14 +12,18 @@ int main(int argc, char **argv) {
     int i = 0;
     char arr[30000];
     memset(arr, 0, sizeof(arr));
-    {}
+    $c_code
+    return 0;
 }
 """
+)
 
 
 def generate(tokens: list[Token]) -> str:
     """Generate valid program on C by given tokens"""
-    return C_CODE.format("\n".join([transpile(token) for token in tokens]))
+    return C_CODE.substitute(
+        c_code="\n".join([transpile(token) for token in tokens])
+    )
 
 
 __all__ = ["generate"]
